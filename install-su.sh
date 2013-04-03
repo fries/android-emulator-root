15c7bd4131b15a8ecae1e0b13de082244bd5d22f
Friedrich Schaeuffelhut <fries@desert.lnp.org>
2013-04-04 01:12:06 +0200

    Fix install.su.sh

--- install-su.sh
+++ install-su.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 adb remount
-adb push apk/su /system/xbin/su
+adb push su/su /system/xbin/su
 #adb shell chmod 06755 /system  
 adb shell chmod 06755 /system/xbin/su   
-adb install -r apk/Superuser-debug.apk
+adb install -r su/Superuser-debug.apk
 
